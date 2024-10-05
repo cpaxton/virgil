3d618dd5af23cf1881d6bf222c1fb2909e43186a
Chris Paxton <paxton.chris@live.com>
2024-10-05 18:05:50 -0400

    updates to quiz setup

--- quiz_maker.py
+++ quiz_maker.py
@@ -17,19 +17,30 @@ Enter something to make a quiz about.
 """
 
 prompt = """
-You are generating a weird personality quiz about {topic}.
+You are generating a weird personality quiz titled, "{topic}".
 
-For each quiz, you will generate {num_questions} questions. There will be 5 multiple-choice options per question: A, B, C, D, and E. At the end, you will also provide a categorization: if the quiz taker chose mostly A, for example, you will describe what A is, and give a description.
+There will be 5 multiple-choice options per question: A, B, C, D, and E. At the end, you will also provide a categorization: if the quiz taker chose mostly A, for example, you will describe what A is, and give a description.
 
-For example, if the topic is "What kind of sandwich are you?" and the first question is "What is your favorite color?" the options might be:
+You will also give a prompt for an image generator associated with each question.
+
+For example:
+
+Topic: What kind of sandwich are you?
+Question 1:
+Question: What is your favorite color?
+Image: a sandwich with a red tomato, a green lettuce leaf, and a yellow cheese slice.
 A. Red
 B. Blue
 C. Green
 D. Yellow
 E. Black
+END QUESTION
+
+End each question with "END QUESTION". Provide no other output.
 
 The quiz is about {topic}.
 
+Topic: {topic}
 Question 1:
 """
 
@@ -41,6 +52,8 @@ def prompt_llm(msg, is_first_message: bool = False):
     if is_first_message:
         is_first_message = False
         full_msg = prompt.format(num_questions=10, topic=topic)
+    else:
+        full_msg = msg
 
     conversation_history.append({"role": "user", "content": full_msg})
 
@@ -56,14 +69,20 @@ def prompt_llm(msg, is_first_message: bool = False):
     # Print the assistant's response
     print(assistant_response)
     print("----------------")
-    print(f"Parse time taken: {parse_dt} seconds")
-    print(f"Story time taken: {t1-t0:.2f} seconds")
+    print(f"Generator time taken: {t1-t0:.2f} seconds")
     print("----------------")
 
 print(userprompt)
 print()
 is_first_message = True
 
-topic = input("Say or do something (empty to quit):")
+topic = input("Enter the title of your dumb personality quiz: ")
 prompt_llm(topic, is_first_message=True)
-prompt_llm("Question 2:")
+for i in range(1,5):
+    prompt_llm(f"Topic: {topic}\nQuestion {i+1}:")
+
+prompt_llm("""If you were mostly A, you were:""")
+prompt_llm("""If you were mostly B, you were:""")
+prompt_llm("""If you were mostly C, you were:""")
+prompt_llm("""If you were mostly D, you were:""")
+prompt_llm("""If you were mostly E, you were:""")
