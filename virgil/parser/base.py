12f13aa5e951ebcd273c5287e6934ad685dd3023
Chris Paxton <paxton.chris@live.com>
2024-10-05 21:00:06 -0400

    update parser

--- virgil/parser/base.py
+++ virgil/parser/base.py
@@ -12,3 +12,30 @@ class Parser(ABC):
     @abstractmethod
     def parse(self, text: str):
         return text
+
+    def prune_to(self, text: str, max_tokens: int):
+        """Prune the text to a maximum number of tokens.
+
+        Args:
+            text (str): The text to prune.
+            max_tokens (int): The maximum number of tokens to prune to.
+
+        Returns:
+            str: The pruned text.
+        """
+        return text[:max_tokens]
+
+    def prune_to_key(self, text: str, key: str):
+        """Prune the text to a key. For example, if the key is "END", the text will be pruned to the first instance of "END".
+
+        Args:
+            text (str): The text to prune.
+            key (str): The key to prune to.
+
+        Returns:
+            str: The pruned text.
+        """
+        idx = text.index(key)
+        if idx == -1:
+            return text
+        return text[:idx]
