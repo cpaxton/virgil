bd3406d991d4b81be8d4813f0bac128e4838d694
Chris Paxton <paxton.chris@live.com>
2024-10-17 18:43:57 -0400

    updating discord bot code

--- virgil/io/discord_bot.py
+++ virgil/io/discord_bot.py
@@ -13,14 +13,57 @@ class DiscordBot:
         # Create an instance of a Client
         self.client = discord.Client(intents=intents)
 
+        self._setup_hooks(self.client)
+
         # Save the token
         self.token = token
+
+    def _setup_hooks(self, client):
+        """Prepare the various hooks to use this Bot object's methods."""
+
+        @client.event
+        async def on_ready():
+            return self.on_ready()
+
+        @client.event
+        async def on_message(message):
+            return self.on_message(message)
+
+    def on_ready(self):
+        """Event listener called when the bot has switched from offline to online."""
+        print(f"{self.client.user} has connected to Discord!")
+        guild_count = 0
+
+        # This is from https://builtin.com/software-engineering-perspectives/discord-bot-python
+        # LOOPS THROUGH ALL THE GUILD / SERVERS THAT THE BOT IS ASSOCIATED WITH.
+        for guild in self.client.guilds:
+            # PRINT THE SERVER'S ID AND NAME.
+            print(f"- {guild.id} (name: {guild.name})")
+
+            # INCREMENTS THE GUILD COUNTER.
+            guild_count = guild_count + 1
+
+        # PRINTS HOW MANY GUILDS / SERVERS THE BOT IS IN.
+        print("This bot is in " + str(guild_count) + " guilds.")
+
+    def on_message(self, message, verbose: bool = False):
+        """Event listener for whenever a new message is sent to a channel that this bot is in."""
+        if verbose:
+            # Printing some information to learn about what this actually does
+            print(message)
+            print("Content =", message.content)
+            print("Content type =", type(message.content))
+
+        text = message.content
+        if text.contains("hello"):
+            message.channel.send("Hello!")
 
     def run(self):
         self.client.run(token)
 
 
 def read_discord_token_from_env():
+    """Helpful tool to get a discord token from the command line, e.g. for a bot."""
     from dotenv import load_dotenv
 
     # Load environment variables from .env file
@@ -32,23 +75,4 @@ def read_discord_token_from_env():
 if __name__ == "__main__":
     token = read_discord_token_from_env()
     bot = DiscordBot(token)
-    client = bot.client
-
-    @client.event
-    async def on_ready():
-        print(f"{client.user} has connected to Discord!")
-
-    @client.event
-    async def on_message(message):
-        print(message)
-        print("Content =", message.content)
-        print("Content type =", type(message.content))
-
-        if message.author == client.user:
-            print(message)
-            return
-
-        if message.content.startswith("$hello"):
-            await message.channel.send("Hello!")
-
     bot.run()
