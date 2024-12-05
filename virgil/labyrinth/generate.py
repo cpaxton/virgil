0452600429c60e4f536e9fe9bb7a3a820cbb33ff
Chris Paxton <paxton.chris@live.com>
2024-12-04 22:05:51 -0500

    add amy's prompt

--- virgil/labyrinth/generate.py
+++ virgil/labyrinth/generate.py
@@ -173,15 +173,15 @@ class LabyrinthGenerator:
             if descriptions[key]["is_start"]:
                 extra_info = " - This is the start room.\n"
             if descriptions[key]["is_goal"]:
-                extra_info = f" - This is the goal room, containing {goal}\n"
+                extra_info = f" - This is the goal room, containing {goal}. Describe it.\n"
             if descriptions[key]["is_dead_end"]:
-                extra_info = " - This is a dead end. You must turn back.\n"
+                extra_info = " - This is a dead end. Describe it.\n"
             if descriptions[key]["is_junction"]:
-                extra_info = " - This is a junction. Multiple paths meet here.\n"
+                extra_info = " - This is a junction. Multiple paths meet here. Describe them.\n"
             if descriptions[key]["has_npc"]:
-                extra_info = " - There is a non-player character here. They seem to be waiting for you. Describe them.\n"
+                extra_info = " - There is someone here, waiting for you. Describe them.\n"
             if descriptions[key]["has_challenges"]:
-                extra_info = " - There is a challenge here. It will be hard to overcome. Describe it.\n"
+                extra_info = " - There is a challenge here that will be hard to overcome. Describe it.\n"
             if descriptions[key]["is_unusual"]:
                 extra_info = " - This place is unusual. It is not like the others. Describe why.\n"
 
