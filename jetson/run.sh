2b00d9439a4c6e9334e8e065971324919eba7f56
Chris Paxton <paxton.chris@live.com>
2024-12-10 16:53:41 -0500

    cleaning up launch file

--- jetson/run.sh
+++ jetson/run.sh
@@ -2,4 +2,5 @@
 # Starts program
 # Installs some dependencies
 # Passes in the discord token
-jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil  -e DISCORD_TOKEN=$DISCORD_TOKEN $(autotag l4t-text-generation) "pip3 install --user termcolor diffusers && export PYTHONPATH=/virgil:$PYTHONPATH && python3 -m virgil.chat"
+jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil  -e DISCORD_TOKEN=$DISCORD_TOKEN $(autotag l4t-text-generation)
+# pip3 install --user termcolor diffusers discord.py && export PYTHONPATH=/virgil:$PYTHONPATH && bash
