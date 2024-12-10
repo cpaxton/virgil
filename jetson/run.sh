e50cb70813f42418ab9c8c5975bfab69c986ebde
Chris Paxton <paxton.chris@live.com>
2024-12-10 16:46:37 -0500

    add token info

--- jetson/run.sh
+++ jetson/run.sh
@@ -1,1 +1,5 @@
-jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil   $(autotag l4t-text-generation) "pip3 install --user termcolor diffusers && export PYTHONPATH=/virgil:$PYTHONPATH && python3 -m virgil.chat"
+#!/usr/bin/env bash
+# Starts program
+# Installs some dependencies
+# Passes in the discord token
+jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil  -e DISCORD_TOKEN=$DISCORD_TOKEN $(autotag l4t-text-generation) "pip3 install --user termcolor diffusers && export PYTHONPATH=/virgil:$PYTHONPATH && python3 -m virgil.chat"
