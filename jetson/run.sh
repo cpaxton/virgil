b4393d50a4960982ac658d239edbe34259f85932
Chris Paxton <paxton.chris@live.com>
2024-12-10 16:59:08 -0500

    more script stuff

--- jetson/run.sh
+++ jetson/run.sh
@@ -3,4 +3,7 @@
 # Installs some dependencies
 # Passes in the discord token
 jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil  -e DISCORD_TOKEN=$DISCORD_TOKEN $(autotag l4t-text-generation)
-# pip3 install --user termcolor diffusers discord.py && export PYTHONPATH=/virgil:$PYTHONPATH && bash
+# pip3 install --user termcolor diffusers discord.py && export PYTHONPATH=/virgil:$PYTHONPATH && python3 -m virgil.friend.friend --backend qwen
+
+# jetson-containers run --volume $HOME/src/virgil:/virgil --workdir /virgil  -e DISCORD_TOKEN=$DISCORD_TOKEN $(autotag l4t-text-generation)
+# pip3 install --user termcolor diffusers discord.py && export PYTHONPATH=/virgil:$PYTHONPATH && python3 -m virgil.friend.friend"
