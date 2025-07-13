30513f59cfbde9fc381d11ef569a3e07eeb2a1ac
Chris Paxton <paxton.chris@live.com>
2025-07-12 22:00:17 -0400

    update

--- scripts/create_banner_for_blog.sh
+++ scripts/create_banner_for_blog.sh
@@ -6,14 +6,14 @@ prompt3="A vibrant and strange solarpunk city, painterly style, sunny day, robot
 prompt4="A vibrant and strange solarpunk city, painterly style, sunny day, robots and humans, crowded streets and markets filled with technology, hanging gardens off of skyscrapers, wind turbines and airships in the background, bright colors, utopian atmosphere" 
 #
 python -m virgil.image.flux --height 800 --width 1200 -n 5 \
-	--output "solarpunk_banner_v04.png" \
+	--output "solarpunk_banner_v05.png" \
 	--prompt "$prompt4"
 
 echo "Done with Flux."
 
 python -m virgil.image.diffuser --height 800 --width 1200 -n 5 \
 	--model "turbo" \
-	--output "solarpunk_banner_diffuser_v04.png" \
+	--output "solarpunk_banner_diffuser_v05.png" \
 	--prompt "$prompt4"
 
 echo "Done with Diffuser."
