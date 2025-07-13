009fe8f1d5d07b76aea5a3209854e1d26fa678c1
Chris Paxton <paxton.chris@live.com>
2025-07-12 21:52:05 -0400

    update

--- scripts/create_banner_for_blog.sh
+++ scripts/create_banner_for_blog.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 #
 prompt1="A vibrant solarpunk city, growing green on buildings, painterly style, sunny day, robots and humans, robots holding hands with human, flowers, hanging gardens, futuristic, solar panels, wind turbines, harmonious coexistence, bright colors, utopian atmosphere" 
-prompt2="A vibrant solarpunk city, growing green on buildings, strange city, painterly style, sunny day, robots and humans, robots holding hands with human, flowers, hanging gardens, futuristic, solar panels, wind turbines, harmonious coexistence, bright colors, utopian atmosphere" 
+prompt2="A vibrant solarpunk city, strange city, painterly style, sunny day, robots and humans, crowded streets and markets, flowers, hanging gardens, futuristic, solar panels, wind turbines, bright colors, utopian atmosphere" 
 #
-python -m virgil.image.flux --height 800 --width 1200 -n 10 \
+python -m virgil.image.flux --height 800 --width 1200 -n 5 \
 	--output "solarpunk_banner_v03.png" \
 	--prompt "$prompt2"
 
 echo "Done with Flux."
 
-python -m virgil.image.diffuser --height 800 --width 1200 -n 10 \
+python -m virgil.image.diffuser --height 800 --width 1200 -n 5 \
 	--model "turbo" \
 	--output "solarpunk_banner_diffuser_v03.png" \
 	--prompt "$prompt2"
